658c0bdee689ceeea8beb204df52ecce4e1fe165
Tom McNamee <tom@tomjmcnamee.com>
2019-03-03 23:41:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ function deleteFromObjectByKey(object, key) {
 function destructivelyDeleteFromObjectByKey(object, key) {
   return delete object.key
 }
+
+function destructivelyDeleteFromObjectByKey
+
+
+
+
+
+

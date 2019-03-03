6b3cb7deb382f7a1f3b82144ca4bd47a8b25a8e7
Tom McNamee <tom@tomjmcnamee.com>
2019-03-03 23:38:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, {[key]: value})
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = delete.
+}
